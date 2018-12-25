da9acf5f9ce3e090b489a49bd4d3c0d466c240de
satwikraman <45010392+satwikraman@users.noreply.github.com>
2018-12-24 16:24:45 -0800

    24-12-18

---
+++ javascript practice/PG10.js
@@ -0,0 +1,11 @@
+function createAccount() {
+    var balance=100
+    return{
+        withdraw:function(amount){
+            balance-=amount
+            console.log(balance)
+        }
+    }
+}
+var account=createAccount()
+account.withdraw(10)
---
+++ javascript practice/StringMethods.js
@@ -0,0 +1,5 @@
+var s="hello world";
+//console.log(s.charCodeAt(1));
+//console.log(s.indexOf("world"));
+//console.log(s.length);
+console.log(s.search("rld"))
