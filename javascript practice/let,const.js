8313aeaf017182b902097a0c0999482c7dbc4c70
satwikraman <45010392+satwikraman@users.noreply.github.com>
2019-01-07 13:28:22 -0800

    07//01/2019

---
+++ javascript practice/let,const.js
@@ -0,0 +1,9 @@
+if(true){
+    var a =10;
+    let b= 15;
+    const c=20;
+    console.log(b);//Local scope
+    console.log(c);//Local scope and cannot be updated
+}
+
+console.log(a);//global scope
