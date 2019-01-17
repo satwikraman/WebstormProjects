5bbf477cc0a91dfd471551436bb36233ba72b2ba
jagadheeswar <jagadheeswarg.ip@gmail.com>
2019-01-16 17:57:59 -0800

    16-01-2019

--- javascript practice/Sumof largest.js
+++ javascript practice/Sumof largest.js
@@ -1,15 +1,19 @@
-var a =[1,0,1]  ;
+var a =[1,0,1];
 var l1=a[0],l2=a[1];
-for(var i in a){
-    if(a[i]>=l1){
+for(let i=0;i<a.length;i++){
+    if(a[i]>l1){
 
         l1=a[i];
     }
-    else if((a[i]>=l2)&&(l1>=12)){
-        l2=a[i];
+    if(l1===a[i+1]){
+        continue;
+    }
+   if((a[i]>=l2)&&(l1!=a[i])){
+       l2=a[i];
+    }
     }
 
 
 
-}
-console.log(l2);
+
+console.log(l1,l2);
