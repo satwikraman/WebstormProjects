b541549342f0bf4e10210c84eb661af0239b02a9
satwik <satwikraman5524@gmail.com>
2019-02-06 08:15:19 -0800

    06-02-2019

--- Nodejs/ReadWrite.js
+++ Nodejs/ReadWrite.js
@@ -3,3 +3,12 @@ fs.readFile('readText.txt','utf8',function(err,data){
     fs.writeFile('WriteText.txt',data);
     console.log(data);
 });
+//fs.unlink('./stuff/writeFile.txt');
+//fs.mkdirSync('STUFF');
+//fs.rmdir('STUFF');
+
+/*fs.mkdir('stuff',function(){
+    fs.readFile('readText.txt','utf8',function(err,data){
+        fs.writeFile('./stuff/writeFile.txt',data);
+    });
+});*/
