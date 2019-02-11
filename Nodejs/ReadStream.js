a17bc43ea7476c062031cbf284204c3e49eaae33
satwik <satwikraman5524@gmail.com>
2019-02-11 14:49:18 -0800

    11-11-2019

--- Nodejs/ReadStream.js
+++ Nodejs/ReadStream.js
@@ -1,6 +1,6 @@
 var http= require('http');
 var fs= require('fs');
-var myReadStream=fs.createReadStream(__dirname+'/ReadStream.txt');
+var myReadStream=fs.createReadStream(__dirname+'/ReadStream.txt','utf8');
 myReadStream.on('data',function(chuck){
     console.log('new chunk recievced:');
     console.log(chuck);
