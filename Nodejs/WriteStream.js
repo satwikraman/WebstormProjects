2bf92da904760cfb14f912801a668e2f1afe9f41
satwik <satwikraman5524@gmail.com>
2019-02-15 14:40:04 -0800

    15-02-2019

---
+++ Nodejs/WriteStream.js
@@ -0,0 +1,8 @@
+var http= require('http');
+var fs= require('fs');
+var myReadStream=fs.createReadStream(__dirname+'/ReadStream.txt','utf8');
+var myWriteStream=fs.createWriteStream(__dirname+'/WriteStream.txt');
+myReadStream.on('data',function(chunk){
+    console.log('new chunk recievced:');
+   myWriteStream.write(chunk);
+});
