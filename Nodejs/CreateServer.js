54c2f9702d9feb620151067bb681b9478f7aadd3
satwik <satwikraman5524@gmail.com>
2019-02-15 16:33:31 -0800

    15-02-2019

--- Nodejs/CreateServer.js
+++ Nodejs/CreateServer.js
@@ -1,10 +1,11 @@
 var http= require('http');
+
 var server=http.createServer(function(req,res){
     //console.log("request was made "+req.url);
     res.writeHead(200,{'content-type':'text/plain'});
     res.write(`url: ${req.url} 
 `);
-    res.end("hai satwik");
+    res.end();
 });
 server.listen(3030,'127.0.0.1');
 console.log('server started');
---
+++ Nodejs/Pipes.js
@@ -0,0 +1,15 @@
+var http= require('http');
+var fs= require('fs');
+var myReadStream=fs.createReadStream(__dirname+'/ReadStream.txt','utf8');
+
+myReadStream.pipe();     
+var server=http.createServer(function(req,res){
+    //console.log("request was made "+req.url);
+    res.writeHead(200,{'content-type':'text/plain'});
+    var myReadStream=fs.createReadStream(__dirname+'/ReadStream.txt','utf8');
+    myReadStream.pipe(res);     
+    
+});
+
+server.listen(3030,'127.0.0.1');
+console.log('server started');
