0c718fe4ef6c19fbaa45fa65c3a6626ad85eca0b
satwik <satwikraman5524@gmail.com>
2019-02-20 10:36:15 -0800

    20-02-2019

--- Nodejs/CreateServer.js
+++ Nodejs/CreateServer.js
@@ -3,8 +3,8 @@ var http= require('http');
 var server=http.createServer(function(req,res){
     //console.log("request was made "+req.url);
     res.writeHead(200,{'content-type':'text/plain'});
-    res.write(`url: ${req.url} 
-`);
+   /* res.write(`url: ${req.url} 
+`);*/
     res.end();
 });
 server.listen(3030,'127.0.0.1');
