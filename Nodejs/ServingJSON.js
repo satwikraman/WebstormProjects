218918fd71163d78594faa93365b68c04cfb8544
satwik <satwikraman5524@gmail.com>
2019-02-21 08:30:00 -0800

    21-02-2019

---
+++ Nodejs/ServingJSON.js
@@ -0,0 +1,13 @@
+var http= require('http');
+var fs= require('fs');
+var server=http.createServer(function(req,res){
+    console.log('request was made');
+    res.writeHead(200,{'Content-Type':'application/json'});
+    var myObj={
+        name:'satwik',
+        age:23,
+        height: 175
+    };
+    res.end(JSON.stringify(myObj));
+});
+server.listen(3030,'127.0.0.1');
