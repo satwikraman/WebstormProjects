b986590893e422ba89cc581abece3e001326617a
satwik <satwikraman5524@gmail.com>
2019-02-26 07:57:19 -0800

    26-02-2019

---
+++ ReactJS/Server.js
@@ -0,0 +1,9 @@
+var http= require('http');
+var fs= require('fs');
+var server=http.createServer(function(req,res){
+    res.writeHead(200,{'content-type':'text/html'});
+    var myReadStream=fs.createReadStream(__dirname+'/index.html','utf8');
+    myReadStream.pipe(res);     
+});
+server.listen(3030,'127.0.0.1');
+console.log('server started');
