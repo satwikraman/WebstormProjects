7e08dffbd6cec05ad91c992c6667917154d4e472
satwik <satwikraman5524@gmail.com>
2019-02-26 14:05:18 -0800

    26-02-2019

--- ReactJS/server.js
+++ ReactJS/server.js
@@ -3,4 +3,7 @@ var app= express();
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html')
 });
+app.get('/form',function(req,res){
+    res.sendFile(__dirname+'/Form.html')
+});
 app.listen(3030);
