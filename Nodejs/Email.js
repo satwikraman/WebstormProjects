1b5e1cb5bc8849e772833c2e61f8f816bf82e735
satwik <satwikraman5524@gmail.com>
2019-02-27 10:11:52 -0800

    27-02-2019

---
+++ Nodejs/Email.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'vivekwhitebox@gmail.com',
+    pass: 'qwertyuiop@1'
+  }
+});
+
+var mailOptions = {
+  from: 'vivekwhitebox@gmail.com',
+  to: 'vivek@whitebox-learning.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
