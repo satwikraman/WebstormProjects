c9b35f6b7c9bbb134155689ef0b42e341d433cbe
satwik <satwikraman5524@gmail.com>
2019-03-14 09:13:16 -0700

    14-03-2019

--- Authentication/app.js
+++ Authentication/app.js
@@ -7,9 +7,20 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 });
 app.post('/',(req,res)=>{
-    console.log(req.body);
-    console.log('data posted succesfully');
+    let ip=req.body;
+    console.log(ip);
+    res.send('data posted succesfully');
 });
+
+app.get('/registration',(req,res)=>{
+    res.sendFile(__dirname+'/registration.html');
+});
+app.post('/registration',(req,res)=>{
+    let rg=req.body;
+    console.log(rg);
+    res.send('Registration succesfull');
+});
+
 var port=4000;
 app.listen(port,()=>{
     console.log(`Running on ${port}`);
