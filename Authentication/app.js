e282a41a489869e32819e622a371872015ac4d65
satwik <satwikraman5524@gmail.com>
2019-03-21 11:31:13 -0700

    21-03-2019

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,11 +1,12 @@
 var express=require('express');
+var bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 var app=express();
 const db=mongoose.connect('mongodb://localhost/Registrationdb');
 const regRoute=express.Router();
 const register=require('./models/schema');
 
-
+app.use(bodyParser.urlencoded({extended:false}));
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 });
@@ -19,8 +20,9 @@ app.get('/registration',(req,res)=>{
     res.sendFile(__dirname+'/registration.html');
 });
 app.post('/registration',(req,res)=>{
+
     let rg=req.body;
-     db.Registrationdb.insert(rg);
+    console.log(rg);
     res.send('Registration succesfull');
 });
 
