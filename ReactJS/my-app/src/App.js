3a075f1d6e7209aa6f5eca0875f829557893ba03
satwik <satwikraman5524@gmail.com>
2019-03-27 15:18:14 -0700

    27-03-2019

--- ReactJS/my-app/src/App.js
+++ ReactJS/my-app/src/App.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
-import Satwik from './Satwik';
+import C1 from './C1';
+
 class App extends Component {
-  state={
-    name:'satwik',
-    age:23
-}
-handleClick=(e)=>{
-    console.log(e.target);
-    this.setState({
-        name:'Satwik Raman',
-        age:23.2
-    })
-}
-handleHover=(e)=>{
-    console.log(e.pageX);
-}
-handleCopy=(e)=>{
-    document.write('copying is prohibited');
-    
-}
   render() {
     return (
-  
-      <div className="app-content">
-            <h1>Hi, Guys</h1>
-            <p>My name is <strong>{this.state.name}</strong>. I am {this.state.age}.</p>
-            <p>{Math.random()*10}</p>
-            <button onClick={this.handleClick}>clickMe</button>
-            <button onMouseOver={this.handleHover}>HoverMe</button>
-            <p onCopy={this.handleCopy}>please don't the text</p>
-            <Satwik />
+      <div className="App">
+        <h1>My First React App</h1>
+        <p>welcome :)</p>
+        <C1 />
       </div>
     );
   }
---
+++ ReactJS/my-app/src/C1.js
@@ -0,0 +1,14 @@
+import React,{component} from 'react';
+
+class C1 extends component{
+    render(){
+        return(
+            <div className="c1">
+                <div>Name: Satwik</div>
+                <div>Age:23</div>
+                <div>Height:5.8</div>
+            </div>
+        )
+    }
+}
+export default C1
