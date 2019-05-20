7554212db56bbde7ef1a9c1319e1762b8b2792bb
satwik <satwikraman5524@gmail.com>
2019-05-20 08:31:28 -0700

    20-05-2019

---
+++ Coding Task/Square.js
@@ -0,0 +1,16 @@
+var b=10
+var obj={
+    a:12,
+    inc:function(){
+        this.a=this.a+1
+        return this.a
+    },
+    dec:()=>{
+        this.b=this.b-1
+        return this.b
+    }
+}
+obj.inc();
+console.log(obj.a)
+console.log(obj.dec())
+console.log(obj.a)
--- ReactJS/react-route/src/components/Post.js
+++ ReactJS/react-route/src/components/Post.js
@@ -1,33 +1,26 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import {connect} from 'react-redux'
 class Post extends Component{
-    state={
-        post:null
-    }
-    componentDidMount(){
-        let id =this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/'+id)
-        .then(resp=>{  
-            this.setState({
-                post: resp.data
-             })
-        })
-    }giafasdg
-render(){
-  const post=this.state.post ? (
-    <div className="post">
-    <h4 className="center">{this.state.post.title}</h4>
-    <p>{this.state.post.body}</p>
-    </div>
-  ):(
-    <div className="post">Loading...</div>
-  )
-  return(
+  render(){
+    const post=this.props.post ? (
+      <div className="post">
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
+      </div>
+    ):(
+      <div className="post">Loading...</div>
+    )
+    return(
       <div className="container">
           {post}
       </div>
-  )
+    )
+  }
 }
+const mapStateToProps=(state,ownProps)=>{
+  let id=ownProps.match.params.post_id
+  return{
+    post: state.posts.find(post=>post.id === id)
+  }
 }
-
-export default Post
+export default connect(mapStateToProps)(Post)
